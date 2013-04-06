ace9b375f254491b8d7d60225b29acef153acf46
biaobiaoqi <shenyapeng@gmail.com>
2013-04-07 02:41:32 +0800

    修改了PAT 1003，还是有两个case通不过

--- src/biaobiaoqi/pat/advancedlevel/APAT1003.java
+++ src/biaobiaoqi/pat/advancedlevel/APAT1003.java
@@ -6,7 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
-
+/**
+ * 测试用例还有2,5没有成功.
+ * @author biaobiaoqi
+ *
+ */
 public class APAT1003 {
 	public static Map<Integer, Vertex> cities = new HashMap<Integer, Vertex>();
 	public static int[] cnt = new int[500];
@@ -30,25 +34,16 @@ public class APAT1003 {
 			
 			cities.get(from).addAdjacent(new Road(to, weight));
 		}
-		
-		//TODO
-		cnt[s] = 1;
-		num[s] = cities.get(s).teams;
-		
 		md(s, d);
-		
-		//TODO
 		System.out.print(cnt[d] + " " + num[d]);
-		
-		
-		//dfs(0, cities.get(d));
-		//System.out.print(allWays + " " + maxTeams);
 	}
 	
 	public static void md(int start, int end) {
+		cnt[start] = 1;
+		num[start] = cities.get(start).teams;
 		cities.get(start).dist = 0;
+		
 		while(true) {
-			//find shortest one in unknow vertexes.
 			int minDist = Integer.MAX_VALUE;
 			Vertex minVertex = null;
 			for (Entry<Integer, Vertex> entry : cities.entrySet()) {
@@ -67,16 +62,8 @@ public class APAT1003 {
 			for (Road r : minVertex.adj) {
 				Vertex newVertex = cities.get(r.dest);
 				if(!newVertex.known) {
-					//< may lose some cases
-//					if (minVertex.dist + r.weight <= newVertex.dist) {
-//						newVertex.dist = minVertex.dist + r.weight;
-//						newVertex.lastPath.add(minVertex);						
-//					}
-					
-					//TODO
 					if (minVertex.dist + r.weight < newVertex.dist) {
 						newVertex.dist = minVertex.dist + r.weight;
-						newVertex.lastPath.add(minVertex);	
 						num[newVertex.id] = num[minVertex.id] + newVertex.teams;
 						cnt[newVertex.id] = cnt[minVertex.id];
 					}else if (minVertex.dist + r.weight == newVertex.dist) {
@@ -90,44 +77,16 @@ public class APAT1003 {
 		}
 	}
 	
-	public static int maxTeams = 0;
-	public static int allWays = 0;
-	
-	//TODO it's not necessary to keep all values in list.
-	//Just the max is ok, and all values will be psed by parameters.
-	public static void dfs(int currentTeams, Vertex v) {
-		currentTeams += v.teams;
-		if (currentTeams > maxTeams) {
-			maxTeams = currentTeams;
-		}
-		
-		if(v.lastPath.size() == 0) {
-			allWays ++;
-			return;
-		}else {
-			for (int i = 0; i != v.lastPath.size(); i ++) {
-				if (i == 0) {
-					dfs(currentTeams, v.lastPath.get(0));
-				}else {
-					dfs(currentTeams, v.lastPath.get(i));
-				}
-			}
-		}
-	}
-	
 	public static class Vertex {
 		public int id;
 		public boolean known;
 		public Integer dist;
 		public int teams;
-		public List<Vertex> lastPath;
-		
-		public List<Road> adj; //TODO array[][]?
+		public List<Road> adj; 
 		
 		public Vertex(int id, int t) {
 			this.id = id;
 			teams = t;
-			lastPath = new ArrayList<Vertex>();
 			dist = Integer.MAX_VALUE;
 			known = false;
 			adj = new ArrayList<Road>();
