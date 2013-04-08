f5bc5a351e2f23f21d35e51ac0c05f20ead9a794
biaobiaoqi <shenyapeng@gmail.com>
2013-04-08 08:50:00 +0800

    重新打了一遍PAT1003，20分钟

--- src/biaobiaoqi/pat/advancedlevel/APAT1003.java
+++ src/biaobiaoqi/pat/advancedlevel/APAT1003.java
@@ -1,61 +1,69 @@
 package biaobiaoqi.pat.advancedlevel;
 
-/*It cost me 40 minutes to make it accepted by PATOJ!!
- * Seconde time.
- * */
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
-
+/**
+ * It's the third time and it takes me 20 minutes to make it accepted by PATOJ.
+ * Actually I've made some mistakes when type in all the code, especially [ and ].
+ * 对于需要一些变形的算法，先打出基本算法。然后再改进，否则容易弄混。
+ * @author biaobiaoqi
+ */
 public class APAT1003 {
 	public static int n, m, from, to;
-	public static int[] c = new int[500];
-	public static int[][] e = new int[500][500];
-	public static boolean[] known = new boolean[500];
-	public static int[] dist = new int[500];
+	public static final int max = 500;
+	public static int[] c = new int[max];
+	public static boolean[] known = new boolean[max];
+	public static int[] dist = new int[max];
+	public static int[][] e = new int[max][max];
 	
-	public static int[] cnt = new int[500];
-	public static int[] tms = new int[500];
-
+	public static int[] cnt = new int[max];
+	public static int[] tms = new int[max];
+	
 	public static void main(String[] args) {
 		Scanner cin = new Scanner(System.in);
 		n = cin.nextInt();
 		m = cin.nextInt();
 		from = cin.nextInt();
 		to = cin.nextInt();
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		Arrays.fill(known, false);
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(e[0], Integer.MAX_VALUE);
+		}
+		
 		for (int i = 0; i < n; i++) {
 			c[i] = cin.nextInt();
 		}
-		int f = 0, t= 0;
-
+		
+		int f = 0, t= 0, w = 0;
 		for (int i = 0; i < m; i++) {
 			f = cin.nextInt();
 			t = cin.nextInt();
-			e[f][t] = e[t][f] = cin.nextInt();
+			w = cin.nextInt();
+			e[f][t] = e[t][f] = w;
+			//TODO else e can be Integer.MAX_VALUE?
 		}
-		//System.out.println(Arrays.toString(e));
-		dijsktra(from, to);
 		
-		System.out.print(cnt[to] + " " + tms[to]);
+		dijkstra(from, to);
+		System.out.println(cnt[to] + " " + tms[to]);
+		
 	}
-
-	public static void dijsktra(int from, int to) {	
-		Arrays.fill(known, false);
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		
+	
+	public static void dijkstra(int from, int to) {
 		dist[from] = 0;
-		tms[from] = c[from];
 		cnt[from] = 1;
+		tms[from] = c[from];
 		
-		while(true){
-			//1.find nearest vertex, mark known
+		while(true) {
+			//1.find min in unkown, mark known
 			int p = -1;
 			int min = Integer.MAX_VALUE;
 			for (int i = 0; i < n; i++) {
-				if (!known[i]) {
-					if (dist[i] < min) {
-						p = i;
-						min = dist[i];
-					}
+				if (!known[i] && dist[i] < min) {
+					min = dist[i];
+					p = i;
 				}
 			}
 			if (p == -1) {
@@ -63,17 +71,17 @@ public class APAT1003 {
 			}
 			known[p] = true;
 			
-			//2.update dist of unknown vertexes
-			for (int j = 0; j < n; j++) {
-				if (e[p][j] != 0 && !known[j]) {
-					if (dist[j] > dist[p] + e[p][j]) { //Wrong:dist[j] > dist[p] + e[p][j]
+			//2.update dist in unknown
+			for(int j = 0; j < n; j++) {
+				if (!known[j] && e[p][j] != 0) {
+					if (dist[j] > dist[p] + e[p][j]) {
 						dist[j] = dist[p] + e[p][j];
 						cnt[j] = cnt[p];
 						tms[j] = tms[p] + c[j];
 					}else if (dist[j] == dist[p] + e[p][j]) {
-						cnt[j] += cnt[p];//Wrong: cnt[j] = cnt[p] + 1; And it's hard to debug.
-						tms[j] = (tms[p] + c[j] > tms[j]) ?tms[p] + c[j] : tms[j]; 
-					}	
+						cnt[j] += cnt[p];
+						tms[j] = (tms[p] + c[j] > tms[j])?tms[p] + c[j] : tms[j];
+					}
 				}
 			}
 		}
