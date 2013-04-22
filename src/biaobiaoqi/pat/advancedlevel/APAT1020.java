dc4d28dd5edf5d0645865dd82c93c0e5b212db4c
biaobiaoqi <shenyapeng@gmail.com>
2013-04-22 22:22:19 +0800

    APAT1020，审题错误

--- src/biaobiaoqi/pat/advancedlevel/APAT1020.java
+++ src/biaobiaoqi/pat/advancedlevel/APAT1020.java
@@ -5,12 +5,11 @@ import java.util.Queue;
 import java.util.Scanner;
 
 /**
- * 1.The inorder array means nothing.
- * 2.Building tree is not nessary.
- * 3.Why it's wa for the last test case.?TODO
+ * 1.#Take care, it's binary tree, instead of binary search tree!!!
+ * 2.Building tree is not necessary£¬ as the process of finding the node is just a level traversal. 
+ * 3.# With a sequence of inorder traversal and another traversal, we can build the tree correctly.  
  * 
  * @author biaobiaoqi
- *
  */
 public class APAT1020 {
 	public static final int max = 31;
@@ -22,42 +21,53 @@ public class APAT1020 {
 		Scanner cin = new Scanner(System.in);
 		n = cin.nextInt();
 		for (int i=0; i < n; ++i) {
-			post[i]= cin.nextInt(); 
+			post[i] = cin.nextInt(); 
 		}
 		for (int i=0; i < n; ++i) {
-			in[i]= cin.nextInt(); 
+			in[i] = cin.nextInt();
 		}
 		
 		Queue<Pair> queue = new LinkedList<Pair>();
-		queue.add(new Pair(0, n-1));
+		queue.add(new Pair(0, n-1, 0, n-1));
 		boolean head = true;
 		while (queue.size() != 0) {
 			Pair p = queue.poll();
-			if (p.start > p.end) continue;
+			if (p.ps > p.pe) continue;
 			
 			if (head) head = false;
 			else System.out.print(" ");
-			System.out.print(post[p.end]);
+			System.out.print(post[p.pe]);
 			
-			if (p.start == p.end) continue;
+			if (p.ps == p.pe) continue;
 			
-			int max = p.start -1;
-			for (int i=p.start; i < p.end; ++i) {
-				if (post[i] < post[p.end]){
-					max = i;
+			int i;
+			for (i = p.is; i < p.ie; ++i) {
+				if (in[i] == post[p.pe]){
+					break;
 				}
 			}
-			queue.add(new Pair(p.start, max));
-			queue.add(new Pair(max+1, p.end-1));
+			queue.add(new Pair(p.ps, 
+					p.ps + i - p.is -1, 
+					p.is, 
+					i - 1));
+			queue.add(new Pair(p.ps + i - p.is,
+					p.pe - 1, 
+					i + 1, 
+					p.ie));
 		}
 	}
 	
 	public static class Pair {
-		public int start;
-		public int end;
-		public Pair(int s, int e) {
-			start = s;
-			end = e;
+		public int ps;
+		public int pe;
+		public int is;
+		public int ie;
+		public Pair(int s, int e, int is, int ie) {
+			ps = s;
+			pe = e;
+			this.is = is;
+			this.ie = ie;
 		}
 	}
+	
 }
