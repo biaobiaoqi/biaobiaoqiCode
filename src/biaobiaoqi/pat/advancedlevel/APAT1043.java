a4941c5c1503826c47ca1956a964bbe277e1334c
biaobiaoqi <shenyapeng@gmail.com>
2013-04-23 09:50:20 +0800

    完成PAT1043

--- src/biaobiaoqi/pat/advancedlevel/APAT1043.java
+++ src/biaobiaoqi/pat/advancedlevel/APAT1043.java
@@ -1,76 +1,77 @@
 package biaobiaoqi.pat.advancedlevel;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
-
+/**
+ * It doesn't need to build a tree, as in some case, a tree can not be defined.
+ * 1.is there any simple way to decide whether it's a bst?
+ * 2. pre -> post order,?
+ * @author biaobiaoqi
+ */
 public class APAT1043 {
 	//define whether it can build a tree
 	public static int n;
 	public final static int max = 1001;
 	public static int[] a = new int[max];
 	public static boolean bst = true;
-	public static Node root = null;
-	public static Stack r = new Stack();
+	public static Queue<Integer> rqueue = new LinkedList<Integer>();
+	public static int mirror;
 	public static void main(String[] args) {
 		Scanner cin = new Scanner(System.in);
 		n = cin.nextInt();
 		for (int i = 0; i < n; ++i) a[i] = cin.nextInt();
 		
-		root = buildTree(0, n - 1);
+		buildTree(0, n - 1, false);
+		if (!bst) {
+			rqueue.clear();
+			bst = true;
+			buildTree(0, n - 1, true);
+		}
 		
 		if(!bst) System.out.println("NO");
 		else {
 			System.out.println("YES");
-			printTree();
+			boolean start = true;
+			
+			while (rqueue.size() != 0) {
+				if (start) start = false;
+				else System.out.print(" ");
+				System.out.print(rqueue.poll());
+			}
 		}
 	}
-	
-	//TODO mirror has not implemented:
+
 	//1.if we can not decide it, it will not affect the result. but how can we build the tree?
 	//2.how can we build the tree.
 	//right start should not be s or e, as if it has not been processed, s or e can lead to infinite loop.
 	//On one hand, it's the start position of right part, on the other hand, it can represent
 	//the existing of right part, and it should can be used to distinguish such a condition.			
-	public static Node buildTree(int s, int e) {
-		if (s > e) return null;
-			
-		Node newNode = new Node(a[s]);
+	public static void buildTree(int s, int e, boolean mirror) {
+		if (s > e) return ;
+		
 		int rightStart = e + 1;
 		for (int i = s + 1; i <= e; i ++) {
-			if (rightStart == e + 1 && a[i] >= a[s]) {
-				rightStart = i;
-			}else if (rightStart != e + 1 && a[i] < a[s]) {
-				bst = false;
+			if (!mirror) {
+				if (rightStart > e  && a[i] >= a[s]) {
+					rightStart = i;
+				}else if (rightStart <= e && a[i] < a[s]) {
+					bst = false;
+				}
+			}
+			else {
+				if (rightStart > e  && a[i] < a[s]) {
+					rightStart = i;
+				}else if (rightStart <= e && a[i] >= a[s]) {
+					bst = false;
+				}
 			}
 		}
 		
-		newNode.left = buildTree(s + 1, rightStart - 1);
-		newNode.right = buildTree(rightStart, e);
-		return newNode;
+		buildTree(s + 1, rightStart - 1, mirror);
+		buildTree(rightStart, e, mirror);
+		rqueue.add(a[s]);
 	}
 	
-	public static void printTree(){
-		postOrder(root);
-	}
-	
-	public static boolean start = true;
-	public static void postOrder(Node n) {
-		if (n.left != null) postOrder(n.left);
-		if (n.right != null) postOrder(n.right);
-		
-		if (start) start = false;
-		else System.out.print(" ");
-		System.out.print(n.value);
-	}
-	
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
-		public Node(int i) {
-			value = i;
-		}
-	}
 }
