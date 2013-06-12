0a6b5b8c6482a021a28b5c7aa323e6a889765294
biaobiaoqi <shenyapeng@gmail.com>
2013-06-12 19:35:47 +0800

    Modify kmp annotation, complete hdoj 1876

--- src/biaobiaoqi/algorithm/string/StringMatching.java
+++ src/biaobiaoqi/algorithm/string/StringMatching.java
@@ -1,65 +1,80 @@
 package biaobiaoqi.algorithm.string;
+
 /**
  * Some test cases:
  * 
  * To test next method: dest:abcabaefabcabc
  * 
- * To test kmp. 
- * dest:abcaba
- * str:abcabcaba
+ * To test kmp. dest:abcaba str:abcabcaba
+ * 
  * @author biaobiaoqi
- *
+ * 
  */
 public class StringMatching {
-	public static int[] next ;
+	public static int[] next;
+
 	public static void main(String[] args) {
 		String str = "BBC ABCDAB ABCDABCDABDE";
 		String destStr = "ABCDABD";
-		
-		System.out.println("bruteforce: " + (bruteforce(str, destStr)?"YES":"NO"));
-		
+
+		System.out.println("bruteforce: "
+				+ (bruteforce(str, destStr) ? "YES" : "NO"));
+
 		next = kmpNext(destStr);
-		System.out.println("kmp: " + (kmp(str, destStr)?"YES":"NO"));
+		System.out.println("kmp: " + (kmp(str, destStr) ? "YES" : "NO"));
 	}
-	
 
 	public static boolean kmp(String str, String dest) {
-		for (int i = 0, j = 0; i < str.length(); i ++) {
+		// i stands for index of str string, j stands for index in dest string.
+		// At the beginning of each loop process, j is the new position of dest
+		// taht should be compared.
+		for (int i = 0, j = 0; i < str.length(); i++) {
 			while (j > 0 && str.charAt(i) != dest.charAt(j))
-				j = next[j - 1];
+				// This loop is to get a matching character recursively. Another
+				// stop condition is when particial match value meets end.
+				j = next[j - 1];// As i in str and j in dest is comparing,
+								// recomputing of j should be in the former
+								// character substring, which is next[j-1]
+
 			if (str.charAt(i) == dest.charAt(j))
-				j ++;
-			
+				j++;
+
 			if (j == dest.length())
 				return true;
 		}
+
 		return false;
 	}
 
 	public static int[] kmpNext(String str) {
 		int[] next = new int[str.length()];
-		next[0]  = 0;
-		for (int i = 1, j = 0; i < str.length(); i ++) {//j == 0 means the cursor points to nothing.
+		next[0] = 0;
+		for (int i = 1, j = 0; i < str.length(); i++) {// j == 0 means the
+														// cursor points to
+														// nothing.
 			while (j > 0 && str.charAt(j) != str.charAt(i))
-				//the j here stands for the number of same characters for postfix and prefix, instead of 
-				//the index of the end of prefix.
-				j = next[j - 1]; //watch out here! it's j - 1 here, instead of j
+				// the j here stands for the number of same characters for
+				// postfix and prefix, instead of
+				// the index of the end of prefix.
+				j = next[j - 1]; // watch out here! it's j - 1 here, instead of
+									// j
 			if (str.charAt(i) == str.charAt(j))
-				j ++;
+				j++;
 			next[i] = j;
 		}
 		return next;
 	}
-	
+
 	public static boolean bruteforce(String str1, String str2) {
-		for (int i = 0, j = 0; i!= str1.length(); ) {
+		for (int i = 0, j = 0; i != str1.length();) {
 			if (str1.charAt(i) == str2.charAt(j)) {
-				j ++;
-				i ++;
-				if (j == str2.length()) return true;
-			}else {		
+				j++;
+				i++;
+				if (j == str2.length())
+					return true;
+			} else {
 				i = i - j + 1;
-				j = 0;					
+				j = 0;
 			}
 		}
 		return false;
--- src/biaobiaoqi/practice/hdoj/HDOJ2203.java
+++ src/biaobiaoqi/practice/hdoj/HDOJ2203.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class HDOJ2203 {
 	public static int[] next = new int[100005];
-	
-	public static void main (String[] atgs) {
+
+	public static void main(String[] atgs) {
 		Scanner cin = new Scanner(System.in);
 		String str = null;
-		String dest= null;
+		String dest = null;
 		while (cin.hasNext()) {
 			str = cin.next();
 			dest = cin.next();
-			if(dest.length() > str.length()) {
+			if (dest.length() > str.length()) {
 				System.out.println("no");
 				continue;
 			}
 			str = str + str;
 			kmpNext(dest);
-			if (kmp(str, dest)) 
+			if (kmp(str, dest))
 				System.out.println("yes");
 			else
 				System.out.println("no");
@@ -27,22 +27,40 @@ public class HDOJ2203 {
 
 	public static void kmpNext(String str) {
 		next[0] = 0;
-		for(int i = 1, j = 0; i < str.length(); ++ i){
-			while (j > 0 && str.charAt(i) != str.charAt(j)) 
-				j = next[j - 1];
-			if (str.charAt(i) == str.charAt(j)) 
-				++ j;
+		// i stands for index of string, j is temporary for particail match
+		// values computing, at the beginning of each loop process, j is the
+		// particial match value of former character .
+		for (int i = 1, j = 0; i < str.length(); ++i) {
+			while (j > 0 && str.charAt(i) != str.charAt(j))
+				// This loop is to get a matching character recursively. Another
+				// stop condition is when particial match value meets end.
+				j = next[j - 1];// j will be recomputed in the recursion. Take
+								// care that next[j-1] is the particial match
+								// value of the first j characters substirng.
+
+			if (str.charAt(i) == str.charAt(j)) // If not in this case, j must
+												// meets end, equals to zero.
+				++j;
+
 			next[i] = j;
 		}
 	}
-	
+
 	public static boolean kmp(String str, String dest) {
-		for (int i = 0, j = 0; i < str.length(); i ++) {
+		// i stands for index of str string, j stands for index in dest string.
+		// At the beginning of each loop process, j is the new position of dest
+		// taht should be compared.
+		for (int i = 0, j = 0; i < str.length(); i++) {
 			while (j > 0 && str.charAt(i) != dest.charAt(j))
-				j = next[j - 1];
+				// This loop is to get a matching character recursively. Another
+				// stop condition is when particial match value meets end.
+				j = next[j - 1];// As i in str and j in dest is comparing,
+								// recomputing of j should be in the former
+								// character substring, which is next[j-1]
+
 			if (str.charAt(i) == dest.charAt(j))
-				j ++;
-			
+				j++;
+
 			if (j == dest.length())
 				return true;
 		}
