23cd760b6e6744f3fc185272ee940ed4257b1a83
biaobiaoqi <shenyapeng@gmail.com>
2013-07-23 21:10:14 +0800

    pat 1018 heap optimization

--- src/biaobiaoqi/algorithm/oj/pat/advancedlevel/APAT1018.cpp
+++ src/biaobiaoqi/algorithm/oj/pat/advancedlevel/APAT1018.cpp
@@ -6,14 +6,14 @@
 	3.计算每条最短路径的带出和带回的车辆数量。
  注意：
 	1.DFS的方法归纳所有最短路径的方法很common sense，需要掌握。
-	2.Dij的对优化方法没有掌握好，写错了，debug了很久。
+	2.Dij的堆优化debug了很久。
 	3.推理证明Dij算法过程中，记录每个节点的带入和带出车辆数量是无法成立的：记n点带入车数量为send[n]，带回车数量为back[n]，证明经过n点的最短路径中下一个节点n+1的send[n+1]和back[n+1]是无法通过send[n] back[n]计算出的，举出反例。
 	因此，必须先得出最短路径，再整体计算。
 */
 #include<stdio.h>
 #include<vector>
-#include<map>
-using namespace::std;
+#include<queue>
+using namespace std;
 #define MAX 501
 #define MAX_INT ((unsigned)(-1) >> 1)
 
@@ -23,41 +23,33 @@ vector<int> prevlist[MAX];
 int c[MAX];
 int cm, n, dest, m;
 vector<vector<int>> result;
-struct distNode{
-	int node;
-	distNode(int a){ node = a; }
-};
-
-bool operator <(const distNode &a, const distNode &b)
-{
-	return dist[a.node] > dist[b.node];
-}
 
 void dijkstra()
 {
 	for (int i = 1; i <= n; ++ i)
 		dist[i] = MAX_INT;
+	typedef pair<int,int>pii;
+	priority_queue<pii,vector<pii>,greater<pii> >q;
+	q.push(make_pair(0, 0));
 
-	while(true) {
-		int minDist = MAX_INT;
-		int selectedNode = -1;
-		for (int i = 0; i != n; ++ i) {
-			if (!visit[i] && minDist > dist[i]) {
-				selectedNode = i;
-				minDist = dist[i];
-			}
-		}
-		if (selectedNode == -1) break;
-		visit[selectedNode] = 1;
+	while(!q.empty()) {
+		int node = q.top().second;
+		int curDist = q.top().first;
+		q.pop();
+
+		if (visit[node])
+			continue;
+		visit[node] = 1;
 
 		for (int i = 0; i <= n; ++ i)
-			if (adj[selectedNode][i] && i != selectedNode && !visit[i] )
-				if (dist[selectedNode] + adj[selectedNode][i] == dist[i])
-					prevlist[i].push_back(selectedNode);
-				else if (dist[selectedNode] + adj[selectedNode][i] < dist[i]) {
-					dist[i] = dist[selectedNode] + adj[selectedNode][i];
+			if (adj[node][i] && i != node && !visit[i])
+				if (dist[node] + adj[node][i] == dist[i])
+					prevlist[i].push_back(node);
+				else if (dist[node] + adj[node][i] < dist[i]) {
+					dist[i] = dist[node] + adj[node][i];
 					prevlist[i].clear();
-					prevlist[i].push_back(selectedNode);
+					prevlist[i].push_back(node);
+					q.push(make_pair(dist[i], i));
 				}
 	}
 }
@@ -116,16 +108,3 @@ int main()
 
 	return 0;
 }
-
-/*
-
-10 3 3 5
-6 7 0
-0 1 1
-0 2 1
-0 3 3
-1 3 1
-2 3 1
-
-3 0->2->3 0
-*/
