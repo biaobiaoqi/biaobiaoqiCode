fac2e924695428815a03156288e46ba9201d63c6
biaobiaoqi <shenyapeng@gmail.com>
2013-07-31 21:19:15 +0800

    pat 1014 refact

--- src/biaobiaoqi/algorithm/oj/pat/advancedlevel/APAT1014.cpp
+++ src/biaobiaoqi/algorithm/oj/pat/advancedlevel/APAT1014.cpp
@@ -1,22 +1,18 @@
 #include<stdio.h>
 #include<queue>
-#include<map>
 #include<stdlib.h>
-#include<string>
 using namespace std;
 
 //注意，在17:00前开始服务的都可以得到结果，而不是17:00前结束的
 
 struct person{
-	int timeLeft;
-	int id;
+	int id, timeLeft; //还需要多长时间的处理，才能完成
 };
 
 int n, m, k, q;
 int t[1001];
 int result[1001];
-queue< person> windows[21];
-int inQueue[11][21];
+queue< person> windows[21];//用queue模拟窗口
 
 #define LIMIT 60*9
 
@@ -31,22 +27,21 @@ int main()
 		windows[i] = pq;
 	}
 
-	int timer = 0;
-	int pLeft = k;
+	int timer = 0;//全局时钟
+	int pLeft = k;//还没有进入黄线内的等待区域的人数
 	while(true){
-		//add to in yellow area.
 		if (timer < LIMIT)
-			for (int i = 0; i != m; ++ i)
+			for (int i = 0; i != m; ++ i) { //按照哪个窗口最短，如果同等队列长度则选编号最小窗口的原则补进黄线外等候的人
 				for (int j = 0; j != n; j ++)
-					if ( inQueue[i][j] == 0 && pLeft > 0){
-						inQueue[i][j] = 1;
+					if (windows[j].size() == i && pLeft > 0) {
 						person p;
 						p.id = k - (pLeft--) + 1;
 						p.timeLeft = t[p.id];
-						windows[j].push(p);
+						windows[j].push(p); //将该人加入队列中。
 					}
+			}
 
-		//get the left ones
+		//找到各个窗口中，下一个离开窗口的时间
 		int min = 0;
 		for (int i = 0; i != n; ++i){
 			if (windows[i].size() != 0) {
@@ -57,18 +52,17 @@ int main()
 
 		timer += min;
 
-		if (min == 0)//end
+		if (min == 0)//如果没有下一个离开窗口的时间，则表明所有人都服务完毕。
 			break;
 
 		for (int i = 0; i != n ; ++ i){
 			if (windows[i].size() != 0){
 				windows[i].front().timeLeft -= min;
-				if (windows[i].front().timeLeft == 0){
-					inQueue[m - 1][i] = 0;
-					result[windows[i].front().id] = timer;
+				if (windows[i].front().timeLeft == 0){ //在timer时间点离开窗口的情况
+					result[windows[i].front().id] = timer; //记录下这个人结束服务的时间
 					windows[i].pop();
 
-					if (timer >= LIMIT){ //If one hasn't been started before LIMIT, then it's sorry case.
+					if (timer >= LIMIT){ //大于LIMIT时间点之后，还没接受处理的人都被清空出队列，不做处理了。
 						while(!windows[i].empty())
 							windows[i].pop();
 					}
@@ -90,9 +84,3 @@ int main()
 	}
 	return 0;
 }
-
-/*
-2 2 7 5
-1 2 6 4 3 534 2
-3 4 5 6 7
-*/
